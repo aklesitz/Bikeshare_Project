1dd7e8c13747c294dca17cb5c46fe548693e58c2
Adam Klesitz <aklesitz@gmail.com>
2023-05-24 14:03:53 -0400

    added start station name

--- blueDashboard.py
+++ blueDashboard.py
@@ -8,14 +8,14 @@ orgDestCSV.columns
 origin = orgDestCSV.drop(columns=["Destination", "End Station Lat", "End Station Long", "End Station"])
 origin.rename(columns={'Origin':'Origin/Destination','Start ID':'Station','Start Latitude':'Latitude',
                        'Start Longtitude':'Longtitude'}, inplace=True)
-origin.drop(columns=['Start Name'], inplace=True)
+#origin.drop(columns=['Start Name'], inplace=True)
 origin.columns
 
 # Create destination csv and rename columns
-destination = orgDestCSV.drop(columns=["Origin", "Start ID", "Start Name", "Start Latitude", "Start Longtitude"])
+destination = orgDestCSV.drop(columns=["Origin", "Start ID", "Start Latitude", "Start Longtitude"])
 destination.rename(columns={'Destination':'Origin/Destination','End Station':'Station','End Station Lat':'Latitude',
                        'End Station Long':'Longtitude'}, inplace=True)
-destination = destination.iloc[:,[0,2,3,1,4,5,6]]
+destination = destination.iloc[:,[0,2,3,1,4,5,6,7]]
 destination.columns
 
 # Combine into one file
@@ -27,7 +27,9 @@ origin_destination.head
 origin_destination[["Origin/Destination", "Station"]] = \
     origin_destination['Origin/Destination'].str.split(' ', expand=True)
 origin_destination.columns
-origin_destination = origin_destination.iloc[:,[0,3,2,5,6,1,4]]
+origin_destination = origin_destination.iloc[:,[0,3,2,5,6,7,1,4]]
+
+origin_destination.isnull().sum()
 
 
 origin_destination.to_csv("origin_destination.csv")
