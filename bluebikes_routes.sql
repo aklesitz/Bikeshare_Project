0cae1b9fa211deab24c02738379b2eea6bf241be
Adam Klesitz <aklesitz@gmail.com>
2024-08-11 11:39:02 -0400

    updating branch

--- bluebikes_routes.sql
+++ bluebikes_routes.sql
@@ -30,26 +30,48 @@ SELECT
 	user_gender
 FROM bluebikes_subscribers;
 
--- Creating table for popular routes
-CREATE TABLE bluebikes_routes AS
+-- Creating table for top 5 routes
+CREATE TABLE top_routes AS
+WITH ranked_routes AS (
+	SELECT
+		start_station_id,
+		end_station_id,
+		starting_lat,
+		starting_long,
+		ending_lat,
+		ending_long,
+		user_type,
+		user_gender,
+		user_birth_year,
+		COUNT(*) AS trip_count,
+		ROW_NUMBER() OVER (PARTITION BY start_station_id ORDER BY COUNT(*) DESC) AS rank
+	FROM
+		combined_rides
+	GROUP BY
+		start_station_id,
+		end_station_id,
+		starting_lat,
+		starting_long,
+		ending_lat,
+		ending_long,
+		user_type,
+		user_gender,
+		user_birth_year
+)
 SELECT
 	start_station_id,
+	end_station_id,
 	starting_lat,
 	starting_long,
-	end_station_id,
 	ending_lat,
 	ending_long,
-	COUNT(*) AS ride_count,
-	user_type
+	trip_count,
+	user_type,
+	user_gender,
+	user_birth_year
 FROM
-	combined_rides
-GROUP BY
-	start_station_id,
-	starting_lat,
-	starting_long,
-	end_station_id,
-	ending_lat,
-	ending_long,
-	user_type
-ORDER BY
-	ride_count DESC;
+	ranked_routes
+WHERE
+	rank <= 5;
+	
+SELECT * FROM top_routes;
